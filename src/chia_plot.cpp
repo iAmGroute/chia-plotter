c5b1e5b189e7986381767e2972971c19325dfa7f
Groute <iAmGroute@outlook.com>
2021-06-17 22:57:21 +0300

    chia_plot.cpp cleanup

--- src/chia_plot.cpp
+++ src/chia_plot.cpp
@@ -17,52 +17,19 @@
 #include <cxxopts.hpp>
 
 #include <string>
-#include <csignal>
 
-#ifndef _WIN32
-#include <sys/resource.h>
-#endif
-
-#ifdef __linux__
-    #include <unistd.h>
-    #define GETPID getpid
-#elif _WIN32
-    #include <processthreadsapi.h>
-    #define GETPID GetCurrentProcessId
-#else
-    #define GETPID() int(-1)
-#endif
-
-bool gracefully_exit = false;
-
-static void interrupt_handler(int sig) {
-    if (!gracefully_exit) {
-        std::cout << std::endl;
-        std::cout << "****************************************************************************************" << std::endl;
-        std::cout << "**  The crafting of plots will stop after the creation and copy of the current plot.  **" << std::endl;
-        std::cout << "**          If you want to resume, press Ctrl-C or send another TERM signal.          **" << std::endl;
-        std::cout << "****************************************************************************************" << std::endl;
-        gracefully_exit = true;
-    } else {
-        std::cout << std::endl;
-        std::cout << "**********************************************************" << std::endl;
-        std::cout << "**********  The crafting of plots will resume.  **********" << std::endl;
-        std::cout << "**********************************************************" << std::endl;
-        gracefully_exit = false;
-    }
-}
+#include <unistd.h>
 
 inline
 phase4::output_t create_plot(    const int num_threads,
                                 const int log_num_buckets,
                                 const vector<uint8_t>& pool_key_bytes,
                                 const vector<uint8_t>& farmer_key_bytes,
-                                const std::string& tmp_dir,
-                                const std::string& tmp_dir_2)
-{
+                                const std::string& tmp_dir
+) {
     const auto total_begin = get_wall_time_micros();
 
-    std::cout << "Process ID: " << GETPID() << std::endl;
+    std::cout << "Process ID: " << getpid() << std::endl;
     std::cout << "Number of Threads: " << num_threads << std::endl;
     std::cout << "Number of Buckets: 2^" << log_num_buckets
             << " (" << (1 << log_num_buckets) << ")" << std::endl;
@@ -150,31 +117,22 @@ int main(int argc, char** argv)
 #endif
         "\n\n"
         "For <poolkey> and <farmerkey> see output of `chia keys show`.\n"
-        "<tmpdir> needs about 220 GiB space, it will handle about 25% of all writes. (Examples: './', '/mnt/tmp/')\n"
-        "<tmpdir2> needs about 110 GiB space and ideally is a RAM drive, it will handle about 75% of all writes.\n"
-        "Combined (tmpdir + tmpdir2) peak disk usage is less than 256 GiB.\n"
-        "In case of <count> != 1, you may press Ctrl-C for graceful termination after current plot is finished.\n"
     );
 
     std::string pool_key_str;
     std::string farmer_key_str;
     std::string tmp_dir;
-    std::string tmp_dir2;
-    std::string final_dir;
-    int num_plots = 1;
     int num_threads = 4;
     int num_buckets = 256;
 
-    options.allow_unrecognised_options().add_options()(
-        "n, count", "Number of plots to create (default = 1, -1 = infinite)", cxxopts::value<int>(num_plots))(
-        "r, threads", "Number of threads (default = 4)", cxxopts::value<int>(num_threads))(
-        "u, buckets", "Number of buckets (default = 256)", cxxopts::value<int>(num_buckets))(
-        "t, tmpdir", "Temporary directory, needs ~220 GiB (default = $PWD)", cxxopts::value<std::string>(tmp_dir))(
-        "2, tmpdir2", "Temporary directory 2, needs ~110 GiB [RAM] (default = <tmpdir>)", cxxopts::value<std::string>(tmp_dir2))(
-        "d, finaldir", "Final directory (default = <tmpdir>)", cxxopts::value<std::string>(final_dir))(
-        "p, poolkey", "Pool Public Key (48 bytes)", cxxopts::value<std::string>(pool_key_str))(
-        "f, farmerkey", "Farmer Public Key (48 bytes)", cxxopts::value<std::string>(farmer_key_str))(
-        "help", "Print help");
+    options.allow_unrecognised_options().add_options() \
+        ("r, threads",   "Number of threads (default = 4)",                        cxxopts::value<int>(num_threads)) \
+        ("u, buckets",   "Number of buckets (default = 256)",                      cxxopts::value<int>(num_buckets)) \
+        ("t, tmpdir",    "Temporary directory, needs ~220 GiB (default = $PWD)",   cxxopts::value<std::string>(tmp_dir)) \
+        ("p, poolkey",   "Pool Public Key (48 bytes)",                             cxxopts::value<std::string>(pool_key_str)) \
+        ("f, farmerkey", "Farmer Public Key (48 bytes)",                           cxxopts::value<std::string>(farmer_key_str)) \
+        ("help",         "Print help") \
+    ;
 
     if (argc <= 1) {
         std::cout << options.help({""}) << std::endl;
@@ -198,15 +156,9 @@ int main(int argc, char** argv)
         std::cout << "tmpdir needs to be specified via -t path/" << std::endl;
         return -2;
     }
-    if (tmp_dir2.empty()) {
-        tmp_dir2 = tmp_dir;
-    }
-    if (final_dir.empty()) {
-        final_dir = tmp_dir;
-    }
     const auto pool_key = hex_to_bytes(pool_key_str);
     const auto farmer_key = hex_to_bytes(farmer_key_str);
-    const int log_num_buckets = num_buckets >= 16 ? int(log2(num_buckets)) : num_buckets;
+    const int log_num_buckets = int(log2(num_buckets));
 
     if (pool_key.size() != bls::G1Element::SIZE) {
         std::cout << "Invalid poolkey: " << bls::Util::HexStr(pool_key) << ", '" << pool_key_str
@@ -222,14 +174,6 @@ int main(int argc, char** argv)
         std::cout << "Invalid tmpdir: " << tmp_dir << " (needs trailing '/' or '\\')" << std::endl;
         return -2;
     }
-    if (!tmp_dir2.empty() && tmp_dir2.find_last_of("/\\") != tmp_dir2.size() - 1) {
-        std::cout << "Invalid tmpdir2: " << tmp_dir2 << " (needs trailing '/' or '\\')" << std::endl;
-        return -2;
-    }
-    if (!final_dir.empty() && final_dir.find_last_of("/\\") != final_dir.size() - 1) {
-        std::cout << "Invalid finaldir: " << final_dir << " (needs trailing '/' or '\\')" << std::endl;
-        return -2;
-    }
     if (num_threads < 1 || num_threads > 1024) {
         std::cout << "Invalid threads parameter: " << num_threads << " (supported: [1..1024])" << std::endl;
         return -2;
@@ -238,72 +182,14 @@ int main(int argc, char** argv)
         std::cout << "Invalid buckets parameter: 2^" << log_num_buckets << " (supported: 2^[4..16])" << std::endl;
         return -2;
     }
-    {
-        const std::string path = tmp_dir + ".chia_plot_tmp";
-        if (auto file = fopen(path.c_str(), "wb")) {
-            fclose(file);
-            remove(path.c_str());
-        } else {
-            std::cout << "Failed to write to tmpdir directory: '" << tmp_dir << "'" << std::endl;
-            return -2;
-        }
-    }
-    {
-        const std::string path = tmp_dir2 + ".chia_plot_tmp2";
-        if (auto file = fopen(path.c_str(), "wb")) {
-            fclose(file);
-            remove(path.c_str());
-        } else {
-            std::cout << "Failed to write to tmpdir2 directory: '" << tmp_dir2 << "'" << std::endl;
-            return -2;
-        }
-    }
-    {
-        const std::string path = final_dir + ".chia_plot_final";
-        if (auto file = fopen(path.c_str(), "wb")) {
-            fclose(file);
-            remove(path.c_str());
-        } else {
-            std::cout << "Failed to write to finaldir directory: '" << final_dir << "'" << std::endl;
-            return -2;
-        }
-    }
-    const int num_files_max = (1 << log_num_buckets) + 20;
 
-#ifndef _WIN32
     {
-        // try to increase the open file limit
         ::rlimit the_limit;
-        the_limit.rlim_cur = num_files_max + 10;
-        the_limit.rlim_max = num_files_max + 10;
+        the_limit.rlim_cur = 5000;
+        the_limit.rlim_max = 5000;
         if (setrlimit(RLIMIT_NOFILE, &the_limit)) {
             std::cout << "Warning: setrlimit() failed!" << std::endl;
         }
-    }
-#endif
-
-    {
-        // check that we can open required amount of files
-        std::vector<std::pair<FILE*, std::string>> files;
-        for (int i = 0; i < num_files_max; ++i) {
-            const std::string path = tmp_dir + ".chia_plot_tmp." + std::to_string(i);
-            if (auto file = fopen(path.c_str(), "wb")) {
-                files.emplace_back(file, path);
-            } else {
-                std::cout << "Cannot open at least " << num_files_max
-                        << " files, please raise maximum open file limit in OS." << std::endl;
-                return -2;
-            }
-        }
-        for (const auto& entry : files) {
-            fclose(entry.first);
-            remove(entry.second.c_str());
-        }
-    }
-
-    if (num_plots > 1 || num_plots < 0) {
-        std::signal(SIGINT, interrupt_handler);
-        std::signal(SIGTERM, interrupt_handler);
     }
 
     std::cout << "Multi-threaded pipelined Chia k32 plotter";
@@ -311,50 +197,8 @@ int main(int argc, char** argv)
         std::cout << " - " << GIT_COMMIT_HASH;
     #endif
     std::cout << std::endl;
-    std::cout << "Final Directory: " << final_dir << std::endl;
-    if (num_plots >= 0) {
-        std::cout << "Number of Plots: " << num_plots << std::endl;
-    } else {
-        std::cout << "Number of Plots: infinite" << std::endl;
-    }
 
-    Thread<std::pair<std::string, std::string>> copy_thread(
-        [](std::pair<std::string, std::string>& from_to) {
-            const auto total_begin = get_wall_time_micros();
-            while (true) {
-                try {
-                    const auto bytes = final_copy(from_to.first, from_to.second);
-
-                    const auto time = (get_wall_time_micros() - total_begin) / 1e6;
-                    std::cout << "Copy to " << from_to.second << " finished, took " << time << " sec, "
-                            << ((bytes / time) / 1024 / 1024) << " MB/s avg." << std::endl;
-                    break;
-                } catch(const std::exception& ex) {
-                    std::cout << "Copy to " << from_to.second << " failed with: " << ex.what() << std::endl;
-                    std::this_thread::sleep_for (std::chrono::minutes(5));
-                }
-            }
-        }, "final/copy");
-
-    for (int i = 0; i < num_plots || num_plots < 0; ++i)
-    {
-        if (gracefully_exit) {
-            std::cout << std::endl << "Process has been interrupted, waiting for copy/rename operations to finish ..." << std::endl;
-            break;
-        }
-        std::cout << "Crafting plot " << i+1 << " out of " << num_plots << std::endl;
-        const auto out = create_plot(num_threads, log_num_buckets, pool_key, farmer_key, tmp_dir, tmp_dir2);
-
-        if (final_dir != tmp_dir)
-        {
-            const auto dst_path = final_dir + out.params.plot_name + ".plot";
-            std::cout << "Started copy to " << dst_path << std::endl;
-            copy_thread.take_copy(std::make_pair(out.plot_file_name, dst_path));
-        }
-    }
-    copy_thread.close();
+    create_plot(num_threads, log_num_buckets, pool_key, farmer_key, tmp_dir);
 
     return 0;
 }
-
-
