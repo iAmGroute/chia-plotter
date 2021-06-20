a72cca393d491c18dbc9cbca86a8bde5fc67ba9c
Groute <iAmGroute@outlook.com>
2021-06-20 19:33:04 +0300

    Revert "Relic backend to GMP"
    This reverts commit 55d21736ce6ee8b7494651664fb74d683332746b.

--- make_release.sh
+++ make_release.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -ffast-math" -DARITH="gmp" -DBUILD_BLS_PYTHON_BINDINGS=false
+cmake .. -DCMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -ffast-math" -DARITH="easy" -DBUILD_BLS_PYTHON_BINDINGS=false
 cmake --build . --config Release -- -j4
