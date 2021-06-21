6336030a6a4a394811960770e3529261a3df8e7f
Groute <iAmGroute@outlook.com>
2021-06-22 01:00:57 +0300

    [debug] buffer allocation printing

--- include/chia/buffer.h
+++ include/chia/buffer.h
@@ -9,6 +9,7 @@
 #define INCLUDE_CHIA_BUFFER_H_
 
 #include <chia/settings.h>
+#include <iostream>
 
 
 template<typename T, size_t _capacity>
@@ -18,6 +19,9 @@ struct byte_buffer_t
     static constexpr size_t entry_size = T::disk_size;
     uint8_t data[capacity * entry_size];
     size_t  count = 0;
+
+    byte_buffer_t()  { std::cout << "B " << _capacity << std::endl; }
+    ~byte_buffer_t() { std::cout << "b " << _capacity << std::endl; }
 
     uint8_t* entry_at(const size_t i) {
         return data + i * entry_size;
